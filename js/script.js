ef393119f1732ef0fb6c574b8e3d49ccd27dbf50
Nazarett <gnazarett@gmail.com>
2015-06-21 01:12:51 -0400

    Return and procedures

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 //Functions - Variable Scope
 
-var width =5;
+/*var width =5;
 
-/*function calcArea(){
+function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
@@ -14,9 +14,33 @@ calcArea(); //This will repeat the whole function*/
 
 //Arguments and Parameters
 
-function dogYears(age){ //**
+/*function dogYears(age){ //**Parameters
     var dogYears = age * 7;
     console.log("Sparky is " + dogYears + " years old.");
 }
 dogYears(4); //**Passing
-dogYears(5);
+dogYears(5); //Arguments*/
+
+//Returning Values
+
+/*var total = calcArea(30, 20); //Make var so return stores there
+
+function calcArea(w, h){
+    var area = w * h;
+    return area; //Function spitting the info out
+}
+console.log(total);*/
+
+//Function vs Procedure
+
+//This is a function
+function calcAreaF(width, height){
+    var area = width * height;
+    return area;
+}
+
+//This is a procedure
+function calcAreaP(width, height){
+    var area = width * height;
+    console.log(area); // no return
+}
