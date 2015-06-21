48cd7078063a98b6bad9a19268a0a73796892748
Nazarett <gnazarett@gmail.com>
2015-06-21 00:58:12 -0400

    Passing

--- js/script.js
+++ js/script.js
@@ -6,17 +6,17 @@ var width =5;
     var width = 20;
     var height = 30;
     var area = width * height;
-    console.log(area);*/
+    console.log(area);
 }
 //console.log(width) //If put console log out of function
                     //Will choose in this case the width outside the function
-calcArea(); //This will repeat the whole function
+calcArea(); //This will repeat the whole function*/
 
 //Arguments and Parameters
 
-calcArea(30, 20);
-
-function calcArea(){
-    var area = w * h;
-    console.log(area);
+function dogYears(age){ //**
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.");
 }
+dogYears(4); //**Passing
+dogYears(5);
