5664781f96ea87e0b7294b38dc9393e8cf418742
Nazarett <gnazarett@gmail.com>
2015-06-21 00:33:16 -0400

    Functions

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//Functions - Variable Scope
+
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
+calcArea(); //This will repeat the whole function
