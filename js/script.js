dc028939c73a737830e360a72c7b1a75c4d36aae
Nazarett <gnazarett@gmail.com>
2015-06-21 00:39:25 -0400

    Console.log

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 //Functions - Variable Scope
 
+var width =5;
 
 function calcArea(){
     var width = 20;
@@ -7,5 +8,8 @@ function calcArea(){
     var area = width * height;
     console.log(area);
 }
-
+//console.log(width) //If put console log out of function
+                    //Will choose in this case the width outside the function
 calcArea(); //This will repeat the whole function
+
+//
