5051582ce110785b95e481953f37953d0bf4291d
Nazarett <gnazarett@gmail.com>
2015-06-21 00:46:00 -0400

    Arguments and parameters

--- js/script.js
+++ js/script.js
@@ -2,14 +2,21 @@
 
 var width =5;
 
-function calcArea(){
+/*function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
-    console.log(area);
+    console.log(area);*/
 }
 //console.log(width) //If put console log out of function
                     //Will choose in this case the width outside the function
 calcArea(); //This will repeat the whole function
 
-//
+//Arguments and Parameters
+
+calcArea(30, 20);
+
+function calcArea(){
+    var area = w * h;
+    console.log(area);
+}
